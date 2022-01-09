f6ee1b0e995996752f52e9963c75801fbb3a7642
JOAN ROA <joanroamora@gmail.com>
2022-01-09 17:30:58 -0500

    pushing the Register Program

---
+++ README.md.txt
@@ -0,0 +1,11 @@
+Register System.
+
+The following program allows access to users contained in an array. 
+The whole exercise is developed in C#. 
+It works as a filter element to allow and deny accesses, 
+depending on the accesses that were set in the initial user 
+planning.
+
+Validation of the existence of a user, in this case within an array.
+The program will welcome the registered user.
+The program will be repeated until a total of ten records in the array are completed.
---
+++ UserLoginProgram/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace UserLoginProgram
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] users = new string[10] { "Lina Maria", "", "", "", "", "", "", "", "", "" };
+            int currentIndex = 1;
+            int startVar;
+            Console.WriteLine("Welcome to our authentication software for Windows. \n \n");
+            while (true)
+            {          
+
+                if (currentIndex < 9)
+                {
+                    Console.WriteLine("Please select one of the next options : \n" + "- For Log in please press one, then enter. \n" + "- For register process please press two, then enter. \n");
+                    startVar = Convert.ToInt32(Console.ReadLine());
+
+                    if (startVar == 1)
+                    {
+                        string yourUser = "";
+                        Console.WriteLine("Please write your user and before press enter.   \n");
+                        yourUser = Console.ReadLine();
+
+                        for (int i = 0; i < users.Length; i++)
+                        {
+                            bool a = Array.Exists(users, element => element == yourUser);
+                            if (a == true)
+                            {
+                                Console.WriteLine("You´re in. Welcome. FUNCIONAAA!");
+                                break;
+                            }
+                            else
+                            {
+                                Console.WriteLine("User not found, please restart the program. ");
+                                Console.WriteLine(users[i]);
+                            }
+                        }
+
+                    }
+
+
+
+                    else if (startVar == 2)
+                    {
+                        Console.WriteLine("Please write your exact username, then press enter key : \n");
+                        users[currentIndex] = Console.ReadLine();
+                        Console.WriteLine($"The user has been created in the slot : {currentIndex} and the username created is {users[currentIndex]}");
+                        currentIndex++;
+                    }
+                }
+
+                else {
+                    Console.WriteLine("Reservations are Over for 2022. Thanks a lot.");
+                }
+                }
+        }
+    }
+}
+
